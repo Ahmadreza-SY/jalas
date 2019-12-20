aa631b96aafe6c0120c2fe6c0565422bcf0060ad
sh-soltanpour <shahryar2006@gmail.com>
2019-12-20 16:27:12 +0330

    Add notificationEnabled to application.properties

--- src/main/kotlin/ir/ac/ut/jalas/services/MailService.kt
+++ src/main/kotlin/ir/ac/ut/jalas/services/MailService.kt
@@ -1,18 +1,26 @@
 package ir.ac.ut.jalas.services
 
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
-class MailService(private val javaMailSender: JavaMailSender) {
+class MailService(
+        private val javaMailSender: JavaMailSender,
+        @Value("\${jalas.notificationEnabled}")
+        val notificationEnabled: Boolean
+) {
 
     private val logger = LoggerFactory.getLogger(javaClass.simpleName)
 
+
     @Async
     fun sendMail(subject: String, message: String, to: String) {
+        if (!notificationEnabled)
+            return
         val mailMessage = SimpleMailMessage()
         mailMessage.setTo(to)
         mailMessage.setSubject(subject)
