b664ddacec3a0a6400bf6208a237c9032382367e
mamareza <mohammad.76kiani@gmail.com>
2019-12-20 16:39:07 +0330

    Fix authentication responses and paths

--- src/main/kotlin/ir/ac/ut/jalas/controllers/AuthController.kt
+++ src/main/kotlin/ir/ac/ut/jalas/controllers/AuthController.kt
@@ -4,28 +4,27 @@ import ir.ac.ut.jalas.auth.JwtTokenUtil
 import ir.ac.ut.jalas.auth.JwtUserDetailsService
 import ir.ac.ut.jalas.controllers.models.JwtRequest
 import ir.ac.ut.jalas.controllers.models.JwtResponse
+import ir.ac.ut.jalas.utils.ErrorType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.DisabledException
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
 
 @RestController
+@RequestMapping("/auth")
 @CrossOrigin
-class JwtAuthenticationController(
+class AuthController(
         val authenticationManager: AuthenticationManager,
         val jwtTokenUtil: JwtTokenUtil,
         val userDetailsService: JwtUserDetailsService
 ) {
-    @PostMapping("/authenticate")
-    fun createAuthenticationToken(@RequestBody authenticationRequest: JwtRequest): ResponseEntity<*> {
+    @PostMapping("/login")
+    fun login(@Valid @RequestBody authenticationRequest: JwtRequest): ResponseEntity<*> {
         authenticate(authenticationRequest.username, authenticationRequest.password)
-        val userDetails = userDetailsService
-                .loadUserByUsername(authenticationRequest.username)
+        val userDetails = userDetailsService.loadUserByUsername(authenticationRequest.username)
         val token = jwtTokenUtil.generateToken(userDetails)
         return ResponseEntity.ok(JwtResponse(token))
     }
@@ -34,9 +33,9 @@ class JwtAuthenticationController(
         try {
             authenticationManager.authenticate(UsernamePasswordAuthenticationToken(username, password))
         } catch (e: DisabledException) {
-            throw Exception("USER_DISABLED", e)
+            throw Exception(ErrorType.USER_DISABLED.name, e)
         } catch (e: BadCredentialsException) {
-            throw Exception("INVALID_CREDENTIALS", e)
+            throw Exception(ErrorType.INVALID_CREDENTIALS.name, e)
         }
     }
 }
