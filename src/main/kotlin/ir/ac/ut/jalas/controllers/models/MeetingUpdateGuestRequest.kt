1af021f834e758665f9bcde433761a8e48fc2e91
mamareza <mohammad.76kiani@gmail.com>
2020-01-08 00:34:40 +0330

    Add guest request validation

--- src/main/kotlin/ir/ac/ut/jalas/controllers/models/MeetingUpdateGuestRequest.kt
+++ src/main/kotlin/ir/ac/ut/jalas/controllers/models/MeetingUpdateGuestRequest.kt
@@ -1,10 +1,12 @@
 package ir.ac.ut.jalas.controllers.models
 
+import javax.validation.constraints.Email
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 data class MeetingUpdateGuestRequest(
         @field:NotNull
         @field:NotBlank
+        @field:Email
         val guest: String
 )
